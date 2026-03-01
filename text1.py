82f7c1f63772a164034bbf24848dcf8230f5829f
伊藤向香 <xiangxiang0058kf@gmail.com>
2026-03-01 12:18:13 +0900

    ほぼ完成

--- text1.py
+++ text1.py
@@ -5,8 +5,8 @@ class Player:
     def __init__(self):
         self.x = pyxel.width / 2
         self.y = pyxel.height - 20
-        self.w = 8
-        self.h = 8
+        self.w = 16
+        self.h = 16
         self.speed = 2
         
     def update(self):
@@ -19,7 +19,7 @@ class Player:
                 bullets.append(Bullet(self.x + self.w /2, self.y))
                 
     def draw(self):
-        pyxel.rect(self.x, self.y, self.w, self.h, 9)
+        pyxel.blt(self.x, self.y, 0, 0, 0, self.w, self.h, 0)
         
 class Bullet:
     def __init__(self, x, y):
@@ -37,7 +37,7 @@ class Enemy:
     def __init__(self):
         self.x = random.randint(0, pyxel.width -8)
         self.y = random.randint(0, pyxel.height / 2)
-        self.vy = 1
+        self.vy = 3
         
     def update(self):
         self.y += self.vy
@@ -46,13 +46,13 @@ class Enemy:
             self.x = random.randint(0, pyxel.width -8)
             
     def draw(self):
-        pyxel.rect(self.x, self.y, 8, 8, 8)
+        pyxel.blt(self.x, self.y, 2, 0, 0, 16, 16, 0)
         
 class Block:
     def __init__(self):
         self.x = random.randint(0, pyxel.width - 8)
         self.y = 0
-        self.vy = random.uniform(1, 3)
+        self.vy = random.uniform(2, 5)
         
     def update(self):
         self.y += self.vy
@@ -62,17 +62,19 @@ class Block:
             self.vy = random.uniform(1, 3)
             
     def draw(self):
-        pyxel.rect(self.x, self.y, 8, 8, 8)
+        pyxel.blt(self.x, self.y, 1, 0, 0, 16, 16, 0)
         
 class App:
     def __init__(self):
         pyxel.init(160, 120, title="シューティングゲーム")
+        pyxel.load("aaaaa.pyxres")
         self.player = Player()
         self.enemies = [Enemy() for _ in range(5)]
         self.blocks = [Block() for _ in range(3)]
         global bullets
         bullets = []
-        self.score = 100
+        self.score = 50
+        self.game_over = False
         pyxel.run(self.update, self.draw)
         
     def update(self):
@@ -93,19 +95,26 @@ class App:
                 
         for block in self.blocks:
             if (self.player.x < block.x + 8 and self.player.x + self.player.w > block.x and self.player.y < block.y + 8 and self.player.y + self.player.h > block.y):
-                self.score -= 1
+                self.score -= 3
                 if self.score < 0:
                     self.score = 0
+                    
+        if self.score < 1:
+            self.game_over = True
                 
     def draw(self):
         pyxel.cls(0)
-        self.player.draw()
-        for bullet in bullets:
-            bullet.draw()
-        for enemy in self.enemies:
-            enemy.draw()
-        for block in self.blocks:
-            block.draw()
-        pyxel.text(5, 5, f"SCORE：{self.score}", 7)
+        if not self.game_over:
+            self.player.draw()
+            for bullet in bullets:
+                bullet.draw()
+            for enemy in self.enemies:
+                enemy.draw()
+            for block in self.blocks:
+                block.draw()
+            pyxel.text(5, 5, f"SCORE：{self.score}", 7)
+        else:
+            pyxel.text(60,50, "GAME OVER", pyxel.frame_count % 16)
+        
             
 App()
