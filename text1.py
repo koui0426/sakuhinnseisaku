90d903064864362daa2e2beb561035d0bbb0bced
伊藤向香 <xiangxiang0058kf@gmail.com>
2026-03-01 12:53:45 +0900

    ほぼ完成パート2

--- text1.py
+++ text1.py
@@ -14,10 +14,16 @@ class Player:
             self.x -= self.speed
         if pyxel.btn(pyxel.KEY_RIGHT):
             self.x += self.speed
-        if pyxel.btn(pyxel.KEY_SPACE):
-            if len(bullets) < 3:
-                bullets.append(Bullet(self.x + self.w /2, self.y))
                 
+        if self.x < 0:
+            self.x = 0
+        if self.x > pyxel.width - self.w:
+            self.x = pyxel.width - self.w
+            
+        if pyxel.btn(pyxel.KEY_SPACE):
+            if len(bullets) < 10:
+                bullets.append(Bullet(self.x + self.w / 2, self.y))
+            
     def draw(self):
         pyxel.blt(self.x, self.y, 0, 0, 0, self.w, self.h, 0)
         
@@ -29,7 +35,9 @@ class Bullet:
     
     def update(self):
         self.y += self.vy
-        
+        if self.y < 0:
+            bullets.remove(self)
+            
     def draw(self):
         pyxel.rect(self.x, self.y, 2, 4, 10)
         
@@ -73,8 +81,9 @@ class App:
         self.blocks = [Block() for _ in range(3)]
         global bullets
         bullets = []
-        self.score = 50
+        self.score = 100
         self.game_over = False
+        self.block_spawn_timer = 0
         pyxel.run(self.update, self.draw)
         
     def update(self):
@@ -86,16 +95,42 @@ class App:
         for block in self.blocks:
             block.update()
             
+        self.block_spawn_timer += 1
+        if self.block_spawn_timer > 30: 
+            self.blocks.append(Block())
+            self.block_spawn_timer = 0   
+            
+        bullets_to_remove = []
+        enemies_to_remove = []
+        blocks_to_remove = []
+            
         for bullet in bullets:
             for enemy in self.enemies:
-                if (enemy.x < bullet.x < enemy.x + 8 and enemy.y < bullet.y < enemy.y + 8):
-                    self.enemies.remove(enemy)
-                    bullets.remove(bullet)
+                if (enemy.x < bullet.x < enemy.x + 16 and enemy.y < bullet.y < enemy.y + 16):
+                    enemies_to_remove.append(enemy)
+                    bullets_to_remove.append(bullet)
                     break
                 
+            for bullet in bullets:
+                for block in self.blocks:
+                    if ((block.x < bullet.x < block.x + 16 and block.y < bullet.y < block.y + 16)):
+                        blocks_to_remove.append(block)
+                        bullets_to_remove.append(bullet)
+                    break
+            
+        for bullet in bullets_to_remove:
+            if bullet in bullets:
+                bullets.remove(bullet)
+        for enemy in enemies_to_remove:
+            if enemy in self.enemies:
+                self.enemies.remove(enemy)
+        for block in blocks_to_remove:
+            if block in self.blocks:
+                self.blocks.remove(block)
+        
         for block in self.blocks:
             if (self.player.x < block.x + 8 and self.player.x + self.player.w > block.x and self.player.y < block.y + 8 and self.player.y + self.player.h > block.y):
-                self.score -= 3
+                self.score -= 1
                 if self.score < 0:
                     self.score = 0
                     
